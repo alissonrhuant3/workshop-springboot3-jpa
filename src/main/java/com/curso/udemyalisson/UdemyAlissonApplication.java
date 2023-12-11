d76ffb13dc58c9c3931c3b5ed0eac7acdedff6e9
alissonrhuant3 <alissonrhuant3@gmail.com>
2023-12-11 18:06:38 -0300

    H2 database, test profile, JPA

--- src/main/java/com/curso/udemyalisson/UdemyAlissonApplication.java
+++ src/main/java/com/curso/udemyalisson/UdemyAlissonApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UdemyAlissonApplication {
+public class 'UdemyAlissonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UdemyAlissonApplication.class, args);
