ba81402ff1097fbc3d5f8bdad6537c7fb144c0ec
alissonrhuant3 <alissonrhuant3@gmail.com>
2023-12-11 23:24:49 -0300

    Service layer, component registration

---
+++ src/main/java/com/curso/udemyalisson/services/UserService.java
@@ -0,0 +1,27 @@
+package com.curso.udemyalisson.services;
+
+import com.curso.udemyalisson.entities.User;
+import com.curso.udemyalisson.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    //Return all users
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    //Return users by Id
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
