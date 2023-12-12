c8c256a9e8f870ceb76573e0bc271b6c58c68314
alissonrhuant3 <alissonrhuant3@gmail.com>
2023-12-12 15:13:01 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/curso/udemyalisson/entities/Product.java
+++ src/main/java/com/curso/udemyalisson/entities/Product.java
@@ -20,7 +20,8 @@ public class Product implements Serializable {
     private Double price;
     private String imgUrl;
 
-    @Transient
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",joinColumns = @JoinColumn(name = "product_id"),inverseJoinColumns = @JoinColumn(name = "category_id")) //Create the table and define the foreign keys of product and category
     private Set<Category> categories = new HashSet<>(); //Set<> A Product cannot repeat the same category
 
     public Product(){
